0275948e891ec02566948ef5f1c6953d6ef3fd4b
heekkang <127381921+heekkang@users.noreply.github.com>
2023-12-16 10:24:56 +0900

    .
    .

--- portfolio/script.js
+++ portfolio/script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const clickableDiv = document.querySelector('.button');
+    const buttonDiv = document.querySelector('.button');
 
     function redirectToNewPage() {
       // 이동하길 원하는 다른 HTML 파일의 경로를 입력하세요.
@@ -10,5 +10,5 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     // div에 클릭 이벤트 추가
-    clickableDiv.addEventListener('click', redirectToNewPage);
+    buttonDiv.addEventListener('click', redirectToNewPage);
   });
