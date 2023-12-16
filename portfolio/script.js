2cb4d4552ea25a73678099280069592c5f4b4f45
heekkang <127381921+heekkang@users.noreply.github.com>
2023-12-16 17:23:50 +0900

    ,
    ,

--- portfolio/script.js
+++ portfolio/script.js
@@ -15,32 +15,4 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 
-  // DOM 요소를 가져옵니다.
-const talkDiv = document.getElementById('talkDiv');
-const container = document.getElementById('container');
 
-let isDragging = false;
-let offsetX, offsetY;
-
-// 마우스 클릭 이벤트를 추가합니다.
-talkDiv.addEventListener('mousedown', (e) => {
-  isDragging = true;
-
-  // 클릭한 위치의 좌표 값을 가져옵니다.
-  offsetX = e.clientX - talkDiv.getBoundingClientRect().left;
-  offsetY = e.clientY - talkDiv.getBoundingClientRect().top;
-});
-
-// 마우스를 놓았을 때 이벤트를 추가합니다.
-document.addEventListener('mouseup', () => {
-  isDragging = false;
-});
-
-// 마우스를 따라 움직이는 함수
-document.addEventListener('mousemove', (e) => {
-  if (isDragging) {
-    // div를 마우스 위치로 이동시킵니다.
-    talkDiv.style.left = (e.clientX - offsetX) + 'px';
-    talkDiv.style.top = (e.clientY - offsetY) + 'px';
-  }
-});
