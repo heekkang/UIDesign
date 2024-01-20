4d0104e3b16eeccc1829874fda512606e50b0305
heekkang <127381921+heekkang@users.noreply.github.com>
2024-01-20 15:31:24 +0900

    .

--- snail/main.js
+++ snail/main.js
@@ -1,16 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
-    let isZoomed = false;
+  let isZoomed = false;
 
-    document.addEventListener('wheel', function(event) {
-      // 스크롤 이벤트 발생 시
-      if (event.deltaY > 0 && !isZoomed) {
-        // 스크롤 다운 및 현재 줌 인 상태가 아닌 경우
-        document.body.classList.add('zoomed');
-        isZoomed = true;
-      } else if (event.deltaY < 0 && isZoomed) {
-        // 스크롤 업 및 현재 줌 인 상태인 경우
-        document.body.classList.remove('zoomed');
-        isZoomed = false;
-      }
-    });
+  window.addEventListener('wheel', function(event) {
+    event.preventDefault(); // 기본 스크롤 이벤트 방지
+
+    const filter1 = document.querySelector('.filter1');
+
+    if (event.deltaY > 0 && !isZoomed) {
+      filter1.classList.add('zoomed');
+      isZoomed = true;
+    } else if (event.deltaY < 0 && isZoomed) {
+      filter1.classList.remove('zoomed');
+      isZoomed = false;
+    }
   });
+});
